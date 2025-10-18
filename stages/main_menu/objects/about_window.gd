d370b2c46201ab5a9833dcb5baeb09dd9a8c868d
jue131 <83157668+jue131@users.noreply.github.com>
2025-10-19 01:47:21 +0900

    translation fixes

--- stages/main_menu/objects/about_window.gd
+++ stages/main_menu/objects/about_window.gd
@@ -3,23 +3,24 @@ extends Window
 @onready var orig_copyright_box: String = %CopyrightBox.text
 @onready var orig_authors_box: String = %AuthorsBox.text
 
-var translation_strings: PackedStringArray = [
-	tr_n(&"Lead Developer of MW:CE:", &"Lead Developers of MW:CE:", 1),
-	tr(&"Graphical Assistance:"),
-	tr(&"Thunder Engine Developers:"),
-	tr(&"Supporters:"),
-	tr(&"Additional Credits:"),
-	tr(&"Softendo/Buziol Games - For the base game"),
-	tr(&"TheMarioVariable - For Mario Worker Remake"),
-	tr(&"English Translation:", &"Put your language here!"),
-	tr(&"Your name", &"Put translation authors here!"),
-]
 
 func _ready() -> void:
-	%CopyrightBox.text = orig_copyright_box % tr(&"A Level Editor for Mario Forever.")
 	update_locale_text.call_deferred()
 	
 func update_locale_text() -> void:
+	%CopyrightBox.text = orig_copyright_box % tr(&"A Level Editor for Mario Forever.")
+	var translation_strings: PackedStringArray = [
+		tr_n(&"Lead Developer of MW:CE:", &"Lead Developers of MW:CE:", 1),
+		tr(&"Graphical Assistance:"),
+		tr(&"Thunder Engine Developers:"),
+		tr(&"Supporters:"),
+		tr(&"Additional Credits:"),
+		tr(&"Softendo/Buziol Games - For the base game"),
+		tr(&"TheMarioVariable - For Mario Worker Remake"),
+		tr(&"English Translation:", &"Put your language here!"),
+		tr(&"Your name", &"Put translation authors here!"),
+	]
+	
 	%AuthorsBox.text = orig_authors_box.format({
 		"lead_dev_title": get_title_colored(translation_strings[0]),
 		"graphics_title": get_title_colored(translation_strings[1]),
@@ -36,6 +37,6 @@ func get_title_colored(text: String) -> String:
 	return "[color=cyan]" + text + "[/color]"
 
 func get_translators(text: String) -> String:
-	if TranslationServer.get_locale() == "en" || "Your name" in text || "English Translation:" in text:
+	if TranslationServer.get_locale() == "en":
 		return ""
 	return text
